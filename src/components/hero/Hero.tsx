9c76a5f2b56ee522aeb8f28caa4a0f64d1860078
Talha Sadiq <talhak911>
2024-06-21 08:48:40 +0500

    increase responsiveness in small screens

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,19 +4,19 @@ import HeroImage from "./HeroImage";
 export default function Hero() {
     return (
         <div className="px-[30px] py-[40px] md:px-[72px]  md:py-[80px] lg:px-[115px] lg:py-[40px]   ">
-            <div className="  flex flex-col md:flex-row gap-[30px] max-w-[1050px] mx-auto  ">
-                <div className=" flex flex-col  gap-[20px] md:w-1/2">
+            <div className="  grid grid-cols-1 md:grid-cols-2 gap-[30px] max-w-[1050px] mx-auto  ">
+                <div className=" flex flex-col  gap-[20px] ">
                     <h1 className="text-white text-[28px] md:text-[38px] lg:text-[67px] font-semibold ">
                         Discover Digital Art & Collect NFTs
                     </h1>
                     <p className="text-white text-[16px] lg:text-[22px] p-1">
                         NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.
                     </p>
-                    <div className='block max-w-[315px] mx-auto md:hidden'>
+                    <div className='block w-full max-w-[315px] mx-auto md:hidden'>
                         <HeroImage />
                     </div>
 
-                    <button className="flex w-[315px] mx-auto md:mx-0 justify-center items-center text-white font-semibold px-12 gap-3 md:w-[224px] h-[60px] bg-[#A259FF] rounded-2xl">
+                    <button className="flex w-full max-w-[315px] mx-auto md:mx-0 justify-center items-center text-white font-semibold px-12 gap-3 md:max-w-[224px] h-[60px] bg-[#A259FF] rounded-2xl">
                         <Rocket /> Get Started
                     </button>
                     <div className='mx-auto md:mx-[0px]'>
@@ -36,7 +36,7 @@ export default function Hero() {
                     </div>
                     </div>
                 </div>
-                <div className='hidden md:flex  md:w-1/2  md:justify-center'>
+                <div className='hidden md:flex  md:justify-center'>
                     <HeroImage />
                 </div>
             </div>
--- src/components/hero/HeroImage.tsx
+++ src/components/hero/HeroImage.tsx
@@ -1,8 +1,8 @@
 import myImage from "../../assets/heroImage.png";
 export default function HeroImage(){
     return (
-        <figure className="flex flex-col rounded-2xl size-[315px] md:size-[330px] lg:size-[510px] ">
-        <img className="h-[206px] md:h-[221px] lg:h-[401px] w-full rounded-t-2xl" src={myImage} alt="Hero image" />
+        <figure className="flex flex-col w-full h-full rounded-2xl max-h-[315px] max-w-[315px]  md:max-h-[330px] md:max-w-[330px] lg:max-h-[510px] lg:max-w-[510px] ">
+        <img className="max-h-[206px] md:max-h-[221px] lg:max-h-[401px] w-full h-full rounded-t-2xl object-cover" src={myImage} alt="Hero image" />
         <figcaption className="h-[109px] bg-[#3B3B3B] p-[20px] text-lg text-white rounded-b-2xl">
             <h5 className="font-semibold">Space Walking</h5>
             <div className="flex">
