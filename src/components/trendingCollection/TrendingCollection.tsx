d82ac9561176644a418730fc1df509fdcd5529fc
Talha Sadiq <talhak911>
2024-07-11 19:23:19 +0500

    paddings

--- src/components/trendingCollection/TrendingCollection.tsx
+++ src/components/trendingCollection/TrendingCollection.tsx
@@ -2,7 +2,7 @@ import DisplayCollections from "../topCollections/TopCollections";
 export default function TrendingCollection(): JSX.Element {
   
   return (
-    <div className="px-[30px] py-[40px] md:px-[72px] md:py-[80px] lg:px-[115px] lg:py-[40px] ">
+    <div className="px-[30px] py-[40px] md:px-[72px] md:py-[40px] lg:px-[115px] lg:py-[80px] ">
       <div className=" max-w-[1050px]  mx-auto flex flex-col gap-[40px] lg:gap-[60px] ">
         <div className="flex flex-col gap-[10px]">
           <h3 className="text-[28px] lg:text-[38px] font-semibold">
