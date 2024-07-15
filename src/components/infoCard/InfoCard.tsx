16cb4cd394486629914df8430f0182994a9899c5
Talha Sadiq <talhak911>
2024-07-15 22:04:18 +0500

    text align center for card

--- src/components/infoCard/InfoCard.tsx
+++ src/components/infoCard/InfoCard.tsx
@@ -7,8 +7,8 @@ export default function InfoCard({ image, title, description }: InfoCardProps): 
     <div className="pt-[10px] md:px-[20px] md:pb-[30px] lg:px-[30px] md:w-[210px] lg:w-[330px] flex md:flex-col items-center gap-[20px] md:gap-[30px] bg-bgSecondary p-[20px] rounded-[20px]">
       <img src={image || imageNotFound} className="size-[100px] md:size-[160px] lg:size-[250px]" alt={title} />
       <div className="flex flex-col gap-[10px]">
-        <h4 className="font-semibold text-[16px] lg:text-[22px]">{title}</h4>
-        <span className="text-[12px] lg:text-[16px]">{description}</span>
+        <h4 className="font-semibold text-[16px] lg:text-[22px] md:text-center">{title}</h4>
+        <span className="text-[12px] lg:text-[16px] md:text-center">{description}</span>
       </div>
     </div>
   );
