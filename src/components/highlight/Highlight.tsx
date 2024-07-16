b9ec328b31b3245d8bc93f9becb77f78905bd123
Talha Sadiq <talhak911>
2024-07-16 23:06:16 +0500

    font change

--- src/components/highlight/Highlight.tsx
+++ src/components/highlight/Highlight.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as EyeIcon } from "../../assets/icons/eye.svg";
 import Timer from "../timer/Timer";
-import image from "../../assets/images/placeholderAvatar.png"
+import image from "../../assets/images/placeholderAvatar.png";
 export default function NftHighlight(): JSX.Element {
   return (
     <div className="relative h-[590px] md:h-[630px] lg:h-[640px] bg-cover bg-center bg-no-repeat bg-nft-mobile md:bg-nft-desktop">
@@ -9,14 +9,12 @@ export default function NftHighlight(): JSX.Element {
           <div className="flex flex-col md:flex-row md:justify-between">
             <div className="flex flex-col gap-[30px]  ">
               <button className="h-[44px] w-[151px] flex justify-center items-center gap-[12px] bg-bgSecondary rounded-[20px]">
-              <img
-              className="size-[24px] rounded-full"
-              src={image}
-              alt=""
-            />
+                <img className="size-[24px] rounded-full" src={image} alt="" />
                 <span> Shroomie</span>
               </button>
-              <h3 className="text-[38px] font-semibold leading-[45.6px]">Magic Mashrooms</h3>
+              <h3 className="text-[38px] lg:text-[51px] lg:leading-[56.1px] font-semibold leading-[45.6px]">
+                Magic Mashrooms
+              </h3>
               <div className="block md:hidden">
                 <Timer isPlaceBid={false} />
               </div>
--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -11,7 +11,7 @@ const Timer: React.FC<{ isPlaceBid: boolean }> = ({
     <div className=" bg-opacity-50 bg-bgSecondary w-full md:w-[295px] h-fit rounded-[30px] p-[30px]">
       <div className="flex justify-center items-center  ">
         <div className="flex justify-center flex-col gap-[10px]">
-          <h4 className="text-[12px] leading-[13.2px]">Auction ends in:</h4>
+          <h4 className="text-[12px] leading-[13.2px] font-mono">Auction ends in:</h4>
           <div className="flex gap-[10px] ">
             <div className="flex flex-col gap-[5px] w-[53px]">
               <div className="flex">
@@ -22,7 +22,7 @@ const Timer: React.FC<{ isPlaceBid: boolean }> = ({
                   {countDownTime?.hours?.charAt(1)}
                 </span>
               </div>
-              <p className=" text-[12px]">Hours</p>
+              <p className=" text-[12px] font-mono">Hours</p>
             </div>
             <span className="text-[38px] font-bold  leading-[45.6px] w-[18px]">:</span>
             <div className="flex flex-col gap-[5px] w-[53px]">
@@ -34,7 +34,7 @@ const Timer: React.FC<{ isPlaceBid: boolean }> = ({
                   {countDownTime?.minutes?.charAt(1)}
                 </span>
               </div>
-              <p className=" text-[12px]">Minutes</p>
+              <p className=" text-[12px] font-mono">Minutes</p>
             </div>
             <span className="text-[38px] font-bold  leading-[45.6px] w-[18px]">:</span>
             <div className="flex flex-col gap-[5px] w-[53px]">
@@ -46,7 +46,7 @@ const Timer: React.FC<{ isPlaceBid: boolean }> = ({
                   {countDownTime?.seconds?.charAt(1)}
                 </span>
               </div>
-              <p className=" text-[12px]">Seconds</p>
+              <p className=" text-[12px] font-mono">Seconds</p>
             </div>
           </div>
           {isPlaceBid && (
