9bd16f3e86018faa1bfa3cd6a822261b5bc5cae8
Talha Sadiq <talhak911>
2024-07-17 08:19:25 +0500

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 import {Colors} from './src/constants/color'
+
 // tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily:{
+      
+    },
     extend: {
       fontFamily: {
         sans: ['Work Sans', 'sans-serif'],
-
+        mono : ['Space Mono'],
+    
       },
 
         colors: {
